943e3fe19ea83bacca7ecc27147e7a48b523b5ad
nalbert_cerqueira <cerqueiranalba6@gmail.com>
2023-08-10 12:47:21 -0300

    first, second, third and fourth line of dev-01 service

---
+++ services/dev-01.ts
@@ -0,0 +1,4 @@
+console.log("first line of dev-01 service");
+console.log("second line of dev-01 service");
+console.log("third line of dev-01 service");
+console.log("fourth line of dev-01 service");
